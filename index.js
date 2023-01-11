841cb007344cc340266a4ee78ec8a56bc7b432cc
Wilsantos1975 <Faustosantos@pursuit.org>
2023-01-11 15:53:08 -0500

    reslve API KEY issue. fetch call 200 k

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-
-
 const cocktailFormSubmit = document.querySelector("form");
 
-
-cocktailFormSubmit.addEventListener("submit", cocktailFormSubmitEvent) // 45 minutes lost because of async
+cocktailFormSubmit.addEventListener("submit", cocktailFormSubmitEvent); // 45 minutes lost because of async //1 hour testing the api, and it was the missing api key.
 
 function cocktailFormSubmitEvent(e) {
   e.preventDefault();
   const input = e.target.cocktail.value;
-  const BASE_URL = `www.thecocktaildb.com/api/json/v1/1/search.php?s=${input}`;
+  const BASE_URL = `http://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=${input}`;
   fetchCocktailInfo(BASE_URL, input);
   e.target.cocktail.value = "";
 }
-function fetchCocktailInfo(BASE_URL, input) {
-  fetch(BASE_URL)
-    .then((res) => {
-      res.json();
-    })
+function fetchCocktailInfo(url, input) {
+  fetch(url)
+    .then((res) => res.json())
     .then((cocktail) => {
-      console.log(cocktail);
-    });
+      console.log(cocktail, input);
+    })
+    .catch((error) => console.log(error));
 }
