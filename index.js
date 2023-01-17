8b8d75f65d19f4c12ed7e7ed44dba0d349f23985
Wilsantos1975 <Faustosantos@pursuit.org>
2023-01-17 17:37:44 -0500

    fixed CORS error

--- index.js
+++ index.js
@@ -1,339 +1,4 @@
-const data = [
-  {
-    idDrink: "13196",
-    strDrink: "Long vodka",
-    strDrinkAlternate: null,
-    strTags: null,
-    strVideo: null,
-    strCategory: "Ordinary Drink",
-    strIBA: null,
-    strAlcoholic: "Alcoholic",
-    strGlass: "Highball glass",
-    strInstructions:
-      "Shake a tall glass with ice cubes and Angostura, coating the inside of the glass. Por the vodka onto this, add 1 slice of lime and squeeze juice out of remainder, mix with tonic, stir and voila you have a Long Vodka",
-    strInstructionsES: null,
-    strInstructionsDE:
-      "Schütteln Sie ein hohes Glas mit Eiswürfeln und Angostura und beschichten Sie so die Innenseite des Glases. Den Wodka darüber gießen, 1 Scheibe Limette hinzufügen und den Saft aus dem Rest herausquetschen, mit Tonic mischen, umrühren und voila, Sie haben einen langen Wodka.",
-    strInstructionsFR: null,
-    strInstructionsIT:
-      "Agitare un bicchiere alto con cubetti di ghiaccio e angostura, ricoprendo l'interno del bicchiere.Versare la vodka su questo, aggiungere 1 fetta di lime e spremere il succo dal resto, mescolare con il tonico, mescolare e voilà hai una Long Vodka",
 
-    strDrinkThumb:
-      "https://www.thecocktaildb.com/images/media/drink/9179i01503565212.jpg",
-    strIngredient1: "Vodka",
-    strIngredient2: "Lime",
-    strIngredient3: "Angostura bitters",
-    strIngredient4: "Tonic water",
-    strIngredient5: "Ice",
-    strIngredient6: null,
-    strIngredient7: null,
-    strIngredient8: null,
-    strIngredient9: null,
-    strIngredient10: null,
-    strIngredient11: null,
-    strIngredient12: null,
-    strIngredient13: null,
-    strIngredient14: null,
-    strIngredient15: null,
-    strMeasure1: "5 cl ",
-    strMeasure2: "1/2 ",
-    strMeasure3: "4 dashes ",
-    strMeasure4: "1 dl Schweppes ",
-    strMeasure5: "4 ",
-    strMeasure6: null,
-    strMeasure7: null,
-    strMeasure8: null,
-    strMeasure9: null,
-    strMeasure10: null,
-    strMeasure11: null,
-    strMeasure12: null,
-    strMeasure13: null,
-    strMeasure14: null,
-    strMeasure15: null,
-    strImageSource: null,
-    strImageAttribution: null,
-    strCreativeCommonsConfirmed: "No",
-    dateModified: "2017-08-24 10:00:12",
-  },
-  {
-    idDrink: "16967",
-    strDrink: "Vodka Fizz",
-    strDrinkAlternate: null,
-    strTags: null,
-    strVideo: null,
-    strCategory: "Other / Unknown",
-    strIBA: null,
-    strAlcoholic: "Alcoholic",
-    strGlass: "White wine glass",
-    strInstructions:
-      "Blend all ingredients, save nutmeg. Pour into large white wine glass and sprinkle nutmeg on top.",
-    strInstructionsES: null,
-    strInstructionsDE:
-      "Alle Zutaten mischen. In ein großes Weißweinglas geben und mit Muskatnuss bestreuen.",
-    strInstructionsFR: null,
-    strInstructionsIT:
-      "Frullare tutti gli ingredienti, tranne la noce moscata.Versare in un grande bicchiere da vino bianco e cospargere di noce moscata.",
-    strDrinkThumb:
-      "https://www.thecocktaildb.com/images/media/drink/xwxyux1441254243.jpg",
-    strIngredient1: "Vodka",
-    strIngredient2: "Half-and-half",
-    strIngredient3: "Limeade",
-    strIngredient4: "Ice",
-    strIngredient5: "Nutmeg",
-    strIngredient6: null,
-    strIngredient7: null,
-    strIngredient8: null,
-    strIngredient9: null,
-    strIngredient10: null,
-    strIngredient11: null,
-    strIngredient12: null,
-    strIngredient13: null,
-    strIngredient14: null,
-    strIngredient15: null,
-    strMeasure1: "2 oz ",
-    strMeasure2: "2 oz ",
-    strMeasure3: "2 oz ",
-    strMeasure4: null,
-    strMeasure5: null,
-    strMeasure6: null,
-    strMeasure7: null,
-    strMeasure8: null,
-    strMeasure9: null,
-    strMeasure10: null,
-    strMeasure11: null,
-    strMeasure12: null,
-    strMeasure13: null,
-    strMeasure14: null,
-    strMeasure15: null,
-    strImageSource: null,
-    strImageAttribution: null,
-    strCreativeCommonsConfirmed: "No",
-    dateModified: "2015-09-03 05:24:03",
-  },
-  {
-    idDrink: "178362",
-    strDrink: "Vodka Slime",
-    strDrinkAlternate: null,
-    strTags: null,
-    strVideo: null,
-    strCategory: "Cocktail",
-    strIBA: null,
-    strAlcoholic: "Alcoholic",
-    strGlass: "Highball glass",
-    strInstructions:
-      "Fill glass with ice. Add vodka, 7-up then finish with the lime juice.",
-    strInstructionsES: null,
-    strInstructionsDE: null,
-    strInstructionsFR: null,
-    strInstructionsIT: null,
-
-    strDrinkThumb:
-      "https://www.thecocktaildb.com/images/media/drink/apex461643588115.jpg",
-    strIngredient1: "Sprite",
-    strIngredient2: "Lime Juice",
-    strIngredient3: "Vodka",
-    strIngredient4: null,
-    strIngredient5: null,
-    strIngredient6: null,
-    strIngredient7: null,
-    strIngredient8: null,
-    strIngredient9: null,
-    strIngredient10: null,
-    strIngredient11: null,
-    strIngredient12: null,
-    strIngredient13: null,
-    strIngredient14: null,
-    strIngredient15: null,
-    strMeasure1: "1 cup",
-    strMeasure2: "1/2 shot",
-    strMeasure3: "1 1/2 shot",
-    strMeasure4: "",
-    strMeasure5: "",
-    strMeasure6: "",
-    strMeasure7: "",
-    strMeasure8: null,
-    strMeasure9: null,
-    strMeasure10: null,
-    strMeasure11: null,
-    strMeasure12: null,
-    strMeasure13: null,
-    strMeasure14: null,
-    strMeasure15: null,
-    strImageSource:
-      "https://giggledam.com/wp-content/uploads/2020/07/4909D8D9-F78E-4391-B7A2-B383CA57D69F.jpeg",
-    strImageAttribution: null,
-    strCreativeCommonsConfirmed: "No",
-    dateModified: null,
-  },
-  {
-    idDrink: "178363",
-    strDrink: "Vodka Lemon",
-    strDrinkAlternate: null,
-    strTags: null,
-    strVideo: null,
-    strCategory: "Cocktail",
-    strIBA: null,
-    strAlcoholic: "Alcoholic",
-    strGlass: "Highball glass",
-    strInstructions:
-      "The vodka lemon is prepared directly in a highball glass or in a large tumbler: put 6-7 ice cubes in the glass, pour the vodka, lemonade and mix with a bar spoon. Finally decorate with a slice of lemon and, if you prefer, add a few mint leaves. Your vodka lemon is ready to be served.",
-    strInstructionsES: null,
-    strInstructionsDE:
-      "Der Wodka Lemon wird direkt in einem Longdrinkglas oder in einem großen Tumbler zubereitet: 6-7 Eiswürfel in das Glas geben, Wodka, Limonade aufgießen und mit einem Barlöffel mixen. Zum Schluss mit einer Zitronenscheibe dekorieren und nach Belieben ein paar Minzeblätter hinzufügen. Ihr Wodka Lemon ist servierfertig.",
-    strInstructionsFR: null,
-    strInstructionsIT:
-      "La vodka lemon si prepara direttamente in un bicchiere highball o in un tumbler grande: mettete nel bicchiere 6-7 cubetti di ghiaccio, versate la vodka, la limonata e mescolate con un bar spoon. Infine decorate con una fetta di limone e, se preferite, aggiungete qualche fogliolina di menta. Il vostro vodka lemon è pronto per essere servito.",
-    strDrinkThumb:
-      "https://www.thecocktaildb.com/images/media/drink/mql55h1643820632.jpg",
-    strIngredient1: "Vodka",
-    strIngredient2: "Lemon Juice",
-    strIngredient3: "Lemon Peel",
-    strIngredient4: "Ice",
-    strIngredient5: null,
-    strIngredient6: null,
-    strIngredient7: null,
-    strIngredient8: null,
-    strIngredient9: null,
-    strIngredient10: null,
-    strIngredient11: null,
-    strIngredient12: null,
-    strIngredient13: null,
-    strIngredient14: null,
-    strIngredient15: null,
-    strMeasure1: "5 cl",
-    strMeasure2: "7 cl",
-    strMeasure3: "1 Slice",
-    strMeasure4: "cubes",
-    strMeasure5: "",
-    strMeasure6: "",
-    strMeasure7: "",
-    strMeasure8: null,
-    strMeasure9: null,
-    strMeasure10: null,
-    strMeasure11: null,
-    strMeasure12: null,
-    strMeasure13: null,
-    strMeasure14: null,
-    strMeasure15: null,
-    strImageSource:
-      "https://blog.giallozafferano.it/lacucinadiliberte/wp-content/uploads/2019/09/Vodka-lemon-d.jpg",
-    strImageAttribution: null,
-    strCreativeCommonsConfirmed: "No",
-    dateModified: null,
-  },
-  {
-    idDrink: "178364",
-    strDrink: "Vodka Tonic",
-    strDrinkAlternate: null,
-    strTags: null,
-    strVideo: null,
-    strCategory: "Cocktail",
-    strIBA: null,
-    strAlcoholic: "Alcoholic",
-    strGlass: "Highball glass",
-    strInstructions:
-      "Wash and cut 1 wedge and 1 slice of lime or lemon.Fill a tumbler with fresh ice.Pour the desired dose of vodka and top up with the tonic. Squeeze the lime wedge into the glass and decorate with the slice.That's all, very simple: it's just the recipe for happiness!",
-    strInstructionsES: null,
-    strInstructionsDE:
-      "1 Keil und 1 Limetten- oder Zitronenscheibe waschen und schneiden.Füllen Sie einen Tumbler mit frischem Eis.Gießen Sie die gewünschte Dosis Wodka ein und füllen Sie das Tonic auf.Limettenschnitz ins Glas drücken und mit der Scheibe dekorieren.Das ist alles, ganz einfach: Es ist einfach das Rezept für Glück!",
-    strInstructionsFR: null,
-    strInstructionsIT:
-      "Lavate e tagliate 1 spicchio e 1 fetta di lime o limone.Colmate un tumbler con ghiaccio fresco.Versate la dose di vodka desiderata e colmate con la tonica.Strizzate lo spicchio di lime dentro il bicchiere e decorate con la fetta.Tutto qui, molto semplice: è solo la ricetta della felicità!",
-
-    strDrinkThumb:
-      "https://www.thecocktaildb.com/images/media/drink/9koz3f1643821062.jpg",
-    strIngredient1: "Vodka",
-    strIngredient2: "Tonic Water",
-    strIngredient3: "Lemon Peel",
-    strIngredient4: null,
-    strIngredient5: null,
-    strIngredient6: null,
-    strIngredient7: null,
-    strIngredient8: null,
-    strIngredient9: null,
-    strIngredient10: null,
-    strIngredient11: null,
-    strIngredient12: null,
-    strIngredient13: null,
-    strIngredient14: null,
-    strIngredient15: null,
-    strMeasure1: "4 cl",
-    strMeasure2: "10 cl",
-    strMeasure3: "1 Slice",
-    strMeasure4: "",
-    strMeasure5: "",
-    strMeasure6: "",
-    strMeasure7: "",
-    strMeasure8: null,
-    strMeasure9: null,
-    strMeasure10: null,
-    strMeasure11: null,
-    strMeasure12: null,
-    strMeasure13: null,
-    strMeasure14: null,
-    strMeasure15: null,
-    strImageSource: "https://www.bernabei.it/media/mixology/vodka%20tonic.jpg",
-    strImageAttribution: null,
-    strCreativeCommonsConfirmed: "No",
-    dateModified: null,
-  },
-  {
-    idDrink: "12800",
-    strDrink: "Coffee-Vodka",
-    strDrinkAlternate: null,
-    strTags: null,
-    strVideo: null,
-    strCategory: "Homemade Liqueur",
-    strIBA: null,
-    strAlcoholic: "Alcoholic",
-    strGlass: "Collins Glass",
-    strInstructions:
-      "Boil water and sugar until dissolved. Turn off heat. Slowly add dry instant coffee and continue stirring. Add a chopped vanilla bean to the vodka, then combine the cooled sugar syrup and coffee solution with the vodka. Cover tightly and shake vigorously each day for 3 weeks. Strain and filter. Its also best to let the sugar mixture cool completely so the vodka won't evaporate when its added. If you like a smoother feel to the liqueur you can add about 1 teaspoon of glycerine to the finished product.",
-    strInstructionsES: null,
-    strInstructionsDE:
-      "Wasser und Zucker kochen, bis sie gelöst sind. Schalten Sie die Heizung aus. Geben Sie langsam trockenen Instantkaffee hinzu und rühren Sie weiter. Dem Wodka eine gehackte Vanilleschote hinzufügen, dann den gekühlten Zuckersirup und die Kaffeelösung mit dem Wodka vermengen. Täglich 3 Wochen lang fest abdecken und kräftig schütteln. Abseihen und filtern. Am besten ist es auch, die Zuckermischung vollständig abkühlen zu lassen, damit der Wodka nicht verdunstet, wenn er hinzugefügt wird. Wenn Sie einen weicheren Geschmack für den Likör mögen, können Sie dem Endprodukt etwa 1 Teelöffel Glyzerin hinzufügen.",
-    strInstructionsFR: null,
-    strInstructionsIT:
-      "Far bollire l'acqua e lo zucchero fino a quando non saranno sciolti.Spegni il fuoco. Aggiungere lentamente il caffè istantaneo secco e continuare a mescolare.Aggiungere un baccello di vaniglia tritato alla vodka, quindi unire lo sciroppo di zucchero raffreddato e la soluzione di caffè con la vodka.Coprire bene e agitare vigorosamente ogni giorno per 3 settimane.Filtrare e filtrare.È anche meglio lasciare raffreddare completamente la miscela di zucchero in modo che la vodka non evapori quando viene aggiunta.Se ti piace una sensazione più liscia al liquore puoi aggiungere circa 1 cucchiaino di glicerina al prodotto finito.",
-
-    strDrinkThumb:
-      "https://www.thecocktaildb.com/images/media/drink/qvrrvu1472667494.jpg",
-    strIngredient1: "Water",
-    strIngredient2: "Sugar",
-    strIngredient3: "Coffee",
-    strIngredient4: "Vanilla",
-    strIngredient5: "Vodka",
-    strIngredient6: "Caramel coloring",
-    strIngredient7: null,
-    strIngredient8: null,
-    strIngredient9: null,
-    strIngredient10: null,
-    strIngredient11: null,
-    strIngredient12: null,
-    strIngredient13: null,
-    strIngredient14: null,
-    strIngredient15: null,
-    strMeasure1: "2 cups ",
-    strMeasure2: "2 cups white ",
-    strMeasure3: "1/2 cup instant ",
-    strMeasure4: "1/2",
-    strMeasure5: "1 1/2 cup",
-    strMeasure6: null,
-    strMeasure7: null,
-    strMeasure8: null,
-    strMeasure9: null,
-    strMeasure10: null,
-    strMeasure11: null,
-    strMeasure12: null,
-    strMeasure13: null,
-    strMeasure14: null,
-    strMeasure15: null,
-    strImageSource: null,
-    strImageAttribution: null,
-    strCreativeCommonsConfirmed: "No",
-    dateModified: "2016-08-31 19:18:14",
-  },
-];
 const cocktailFormSubmit = document.querySelector("form");
 const main = document.querySelector("main");
 
@@ -342,36 +7,31 @@ cocktailFormSubmit.addEventListener("submit", cocktailFormSubmitEvent); // 45 mi
 function cocktailFormSubmitEvent(e) {
   e.preventDefault();
   const input = e.target.cocktail.value;
-  const BASE_URL = `http://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=${input}`;
+  const BASE_URL = `https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=${input}`;
   fetchCocktailInfo(BASE_URL, input);
   e.target.cocktail.value = "";
 }
-function fetchCocktailInfo(url, input) {
-  fetch(url)
-    .then((res) => res.json())
-    .then((cocktail) => {
-      handleCocktail(cocktail, input);
-    })
-    .catch((error) => console.log(error));
-}
 
-// async function fetchCocktailInfo(url, input) { //function written as async await, to test the api
-//   const response = await fetch(url);
-//   const data = await response.json();
-//   console.log(data)
+
+// function fetchCocktailInfo(url, input) {
+//   console.log(url)
+//   fetch(url)
+//     .then((res) => res.json())
+//     .then((cocktail) => {
+//       handleCocktail(cocktail, input);
+//     })
+//     .catch((error) => console.log(error));
 // }
 
-function handleCocktail(cocktail, input) {
-  cocktail.drinks.length > 1 ? manyCocktails : singleCocktail;
+async function fetchCocktailInfo(url, input) { //function written as async await, to test the api
+  const response = await fetch(url);
+  const cocktail = await response.json();
+  handleCocktail(cocktail, input);
+}
 
-  //   if (cocktail.drinks.length > 1) {
-  //     manyCocktails();
-  //   } else {
-  //     let h1 = document.createElement("h1");
-  //     h1.textContent = cocktail.drinks[0].strDrink;
-  //     main.append(h1);
-  //     console.log(cocktail);
-  //   }
+function handleCocktail(cocktail, input) {
+  cocktail.drinks.length > 1 ? manyCocktails(cocktail,input) : singleCocktail(cocktail,input);
+  
 }
 
 function manyCocktails(cocktail, input) {
@@ -389,21 +49,6 @@ function singleCocktail(cocktail) {
   let h1 = document.createElement("h1");
   h1.textContent = cocktail.drinks[0].strDrink;
   main.append(h1);
-  console.log(cocktail);
+  
 }
 
-//logic below is is for hamburguer menu;
-
-const hamburguer = document.querySelector(".hamburguer");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburguer.addEventListener("click", () => {
-  hamburguer.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
-document.querySelectorAll(".nav-link").forEach((link) => {
-  addEventListener("click", () => {
-    hamburguer.classList.remove("active");
-    navMenu.classList.remove("active");
-  });
-});
